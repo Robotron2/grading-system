2057feef9ad9ce5ea19bae62691c125dd3046c88
Adeleke Theophilus <theophilusadesola002@gmail.com>
2022-09-28 10:12:14 +0100

    Favicon, title style completed

--- index.js
+++ index.js
@@ -4,18 +4,31 @@ let displayDiv = document.getElementById("displayDiv")
 
 scoreBtn.addEventListener("click", () => {
     if (inputScore.value == "") {
-        displayDiv.innerHTML += `<h4>Field cannot be empty</h4>`
+        displayDiv.innerHTML = `<h4 class = "text-danger">Field Cannot be empty</h4>`
+        inputScore.value = ""
+    } else if (inputScore.value > 100) {
+        displayDiv.innerHTML = `<h4 class = "text-dark">Enter scores between 0 and 100</h4>`
+        inputScore.value = ""
     } else if (inputScore.value >= 70 && inputScore.value <= 100) {
-        displayDiv.innerHTML += `A`
+        displayDiv.innerHTML = `<h4 class = "text-success">Grade is A. Keep it Up!</h4>`
+        inputScore.value = ""
     } else if (inputScore.value >= 60 && inputScore.value <= 69) {
-        displayDiv.innerHTML += `B`
+        displayDiv.innerHTML = `<h4>Grade is B. Almost there!!</h4>`
+        inputScore.value = ""
     } else if (inputScore.value >= 50 && inputScore.value <= 59) {
-        displayDiv.innerHTML += `C`
+        displayDiv.innerHTML = `<h4>Grade is C. Put more effort!</h4>`
+        inputScore.value = ""
     } else if (inputScore.value >= 40 && inputScore.value <= 49) {
-        displayDiv.innerHTML += `D`
+        displayDiv.innerHTML = `<h4>Grade is D. Work more get C!!</h4>`
+        inputScore.value = ""
     } else if (inputScore.value >= 30 && inputScore.value <= 39) {
-        displayDiv.innerHTML += `E`
+        displayDiv.innerHTML = `<h4>Grade is E. Improve!!</h4>`
+        inputScore.value = ""
     } else if (inputScore.value >= 0 && inputScore.value <= 29) {
-        displayDiv.innerHTML += `F`
+        displayDiv.innerHTML = `<h4 class = "text-danger">Grade is F. Try harder next time!!</h4>`
+        inputScore.value = ""
+    } else {
+        displayDiv.innerHTML = `<h4 class = "text-danger">Enter real numbers only</h4>`
+        inputScore.value = ""
     }
 })
